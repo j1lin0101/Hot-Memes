583d6dafebcd550dbd167ebb0ed5eb095ef0e27a
Jeremy <j1lin0101@gmail.com>
2022-03-13 15:04:40 -0700

    Accordion Components

---
+++ components/AboutAccordion.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "about-accordion",
+  props: ["accordionTitle", "accordionDescription", "accordionId"],
+};
+</script>
+
+<template>
+  <div>
+    <div class="mb-3">
+      <b-button v-b-toggle="'my-collapse' + accordionId">{{
+        accordionTitle
+      }}</b-button>
+    </div>
+
+    <b-collapse :id="'my-collapse' + accordionId">
+      <b-card :title="accordionTitle">{{ accordionDescription }}</b-card>
+    </b-collapse>
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,37 @@
+<script>
+import AboutAccordion from "../components/AboutAccordion.vue";
+
+export default {
+  components: {
+    AboutAccordion,
+  },
+  data() {
+    return {
+      aboutDetails: [
+        {
+          id: 1,
+          title: "Who We Are",
+          content:
+            "The Hot Meme Network is run by a guy with way too much time on his hands who also spends way too much time on his phone. This knowledge that he has could be used to serve the greater world and its problems. Instead he wastes his time on memes on Reddit.",
+        },
+        {
+          id: 2,
+          title: "Why Hot Memes",
+          content:
+            "One day I found a particular meme very histerical. Wanting this to be a moment to connect with my mother, I shared it with her. Instead of laughing, she gave me a very concerned and sympathetic look. 'How do you find this funny?' It occurred to me at that moment that our senses of humor were out of sync. I wanted to give her a breakdown of why it was funny, but it occurred to me that meme history is so deep and lore-filled for no apparently good reason. The Hot Meme Network functions as that bridge between history and now. Giving an abridged version of internet culture with the intent of catching people up and bringing the culture gap closer.",
+        },
+        {
+          id: 3,
+          title: "How can I support?",
+          content: "Buy me a coffee! Venmo at jlin0104",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
 <template>
   <div class="container py-4">
     <b-nav pills class="pb-3 mb-4 border-bottom">
@@ -9,63 +43,16 @@
     <h1>About Us</h1>
 
     <!-- Accordion with Descriptions -->
-    <div class="accordion" id="accordionExample">
-      <!-- Boostrap Accordion with Vue Dynamic Items from https://therichpost.com/vue-3-bootstrap-5-accordion-with-dynamic-items/ -->
-      <div
-        class="accordion-item"
-        v-for="(details, index) in aboutDetails"
-        :key="index"
-      >
-        <h2 class="accordion-header" :id="'heading' + index">
-          <button
-            class="accordion-button"
-            :class="{ collapsed: index !== 0 }"
-            type="button"
-            data-bs-toggle="collapse"
-            :data-bs-target="'#collapse' + index"
-            aria-expanded="true"
-            :aria-controls="'collapse' + index"
-          >
-            {{ details.title }}
-          </button>
-        </h2>
-        <div
-          :id="'collapse' + index"
-          class="accordion-collapse collapse"
-          :class="{ show: index === 0 }"
-          :aria-labelledby="'heading' + index"
-          data-bs-parent="#accordionExample"
-        >
-          <div class="accordion-body">
-            {{ details.content }}
-          </div>
-        </div>
-      </div>
+    <div>
+      <about-accordion
+        v-for="detail in aboutDetails"
+        v-bind:detaill="detail"
+        v-bind:key="detail.id"
+        v-bind:accordionId="detail.id"
+        v-bind:accordionTitle="detail.title"
+        v-bind:accordionDescription="detail.content"
+      ></about-accordion>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      aboutDetails: [
-        {
-          title: "Who We Are",
-          content:
-            "The Hot Meme Network is run by a guy with way too much time on his hands who also spends way too much time on his phone. This knowledge that he has could be used to serve the greater world and its problems. Instead he wastes his time on memes on Reddit.",
-        },
-        {
-          title: "Why Hot Memes",
-          content:
-            "One day I found a particular meme very histerical. Wanting this to be a moment to connect with my mother, I shared it with her. Instead of laughing, she gave me a very concerned and sympathetic look. 'How do you find this funny?' It occurred to me at that moment that our senses of humor were out of sync. I wanted to give her a breakdown of why it was funny, but it occurred to me that meme history is so deep and lore-filled for no apparently good reason. The Hot Meme Network functions as that bridge between history and now. Giving an abridged version of internet culture with the intent of catching people up and bringing the culture gap closer.",
-        },
-        {
-          title: "How can I support?",
-          content: "Buy me a coffee! Venmo at jlin0104",
-        },
-      ],
-    };
-  },
-};
-</script>
